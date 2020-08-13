3f03fb8315d51a2bd62ae20e0240bbd87e54758e
drafter500 <vzemtsov@yahoo.com>
2020-08-13 13:34:17 +0200

    UP-425 - Creates initialize function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export function hello(what: string): string {
-  return `hello ${what}`;
+import { v4 as uuidv4 } from 'uuid';
+
+
+const partnerIdKey = Symbol('partnerId');
+const sessionIdKey = Symbol('sessionId');
+
+export class ThirtyFiveUp {
+  [partnerIdKey]: string;
+  [sessionIdKey]: string;
+
+  constructor(partnerId: string, sessionId: string) {
+    this[partnerIdKey] = partnerId;
+    this[sessionIdKey] = sessionId;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  getProductRecommendations(): void {
+
+  }
+}
+
+export function initialise(partnerId: string, sessionId?: string)
+  : ThirtyFiveUp {
+  return new ThirtyFiveUp(partnerId, sessionId || uuidv4());
 }
