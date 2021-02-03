da2d47fa926e3fcdf7e5a4ef0ed354e9220afd97
Uschi <uschi.schrod@gmail.com>
2021-02-03 15:09:58 +0100

    UP-1014 - Fixes test

--- src/services/recommendations.test.ts
+++ src/services/recommendations.test.ts
@@ -29,7 +29,7 @@ const productRecommendations = getMockRecommendations();
 describe('service - recommendations', () => {
   describe('makeSearchParams', () => {
     it('converts object to search string', () => {
-      const expected = 'partner=partner-7&session=sess-12&lang=en&country=us'
+      const expected = 'partner=partner-7&session=sess-12&lang=en&country=de'
         + '&limit=10&baseProduct.title=Cocobolo%20desk&baseProduct.category'
         + '=Furniture&baseProduct.extra.foo=ba-da%20boo%5E%3F&customer.age='
         + '20-30&customer.cities=Berlin,Frankfurt%20am%20Main';
