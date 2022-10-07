3b0035dd5ff25fd49b44a60a0960bf3c85615c7d
drafter500 <vzemtsov@yahoo.com>
2022-10-07 17:45:36 +0200

    UP-3651 - Polishes node package

--- packages/node/rollup.config.js
+++ packages/node/rollup.config.js
@@ -1,27 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 // eslint-disable-next-line import/extensions
 import pkg from './package.json';
 
 
-export default [{
+export default {
   input: './index.ts',
   output: [
     {file: pkg.module, format: 'es'},
-    {
-      file: pkg.module.replace(/.js$/, '.min.js'),
-      format: 'es',
-      plugins: [terser()],
-    },
     {file: pkg.main, format: 'cjs', name: 'thirtyFiveUp'},
-    {
-      file: pkg.main.replace(/.js$/, '.min.js'),
-      format: 'cjs',
-      name: 'thirtyFiveUp',
-      plugins: [terser()],
-    },
   ],
   external: Object.keys(pkg.dependencies || {}),
   plugins: [
@@ -29,17 +17,4 @@ export default [{
     nodeResolve({browser: false}),
     typescript(),
   ],
-},
-{
-  input: './index.ts',
-  output: [
-    {file: pkg.iife, format: 'iife', name: 'thirtyFiveUp'},
-    {file: pkg.amd, format: 'amd', name: 'thirtyFiveUp'},
-  ],
-  plugins: [
-    commonjs(),
-    nodeResolve({browser: true}),
-    typescript(),
-    terser(),
-  ],
-}];
+};
