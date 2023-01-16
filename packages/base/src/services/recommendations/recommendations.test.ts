62250fa53c283eeeb7cc00ca3650d9cd4bf4fa6e
Fausto Arturo Guerrero Velez <enjoythelive1@gmail.com>
2023-01-16 12:32:14 +0100

    UP-4015 - Updates @35up/tslib-utils to include stripUndefined

--- packages/base/src/services/recommendations/recommendations.test.ts
+++ packages/base/src/services/recommendations/recommendations.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import fetch from 'jest-fetch-mock';
+import { stripUndefined } from '@35up/tslib-utils';
 import { makeSearchParams, getProductRecommendations } from './recommendations';
 import { GetRecommendationsParams, SdkConfig } from '../../types';
 import { getMockRecommendations } from './recommendations-mock-data';
@@ -47,7 +48,7 @@ describe('service - recommendations', () => {
 
       expect(makeSearchParams({
         ...sdkConfigWithoutUrl,
-        ...input,
+        ...stripUndefined(input),
       })).to.equal(expected);
     });
   });
